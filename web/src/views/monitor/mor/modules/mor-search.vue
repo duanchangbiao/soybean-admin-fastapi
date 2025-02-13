dcca40273bf10eb21bc6ab4c89343d9c28e3952d
duanchangbiao@zhangkongapp.com <duan123456@>
2025-02-13 12:30:45 +0800

    新增数据权限控制监控表信息

--- web/src/views/monitor/mor/modules/mor-search.vue
+++ web/src/views/monitor/mor/modules/mor-search.vue
@@ -40,9 +40,9 @@ async function search() {
           <NInput v-model:value="model.applyNumber"
                   :placeholder="$t('page.business.mor.form.applyNumber')"/>
         </NFormItemGi>
-        <NFormItemGi span="24 s:8 m:4" :label="$t('page.business.mor.nickName')" path="username"
+        <NFormItemGi span="24 s:8 m:4" :label="$t('page.business.mor.nickname')" path="username"
                      class="pr-24px">
-          <NInput v-model:value="model.nickName" :placeholder="$t('page.business.mor.form.nickName')"/>
+          <NInput v-model:value="model.nickName" :placeholder="$t('page.business.mor.form.nickname')"/>
         </NFormItemGi>
         <NFormItemGi span="24 s:8 m:4" :label="$t('page.business.mor.applyStatus')" path="applyStatus"
                      class="pr-24px">
